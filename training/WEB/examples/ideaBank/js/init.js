cec5af34ba497664131e914873dc2dfb3dae690a
John Garcia <johnkegd@gmail.com>
2022-01-26 22:50:49 +0100

    [reorder] - ideaBank repo moved to web examples and deleted
    Former-commit-id: e4b69faecb8195e33a7544dfe4bead5d23889827

---
+++ training/WEB/examples/ideaBank/js/init.js
@@ -0,0 +1,14 @@
+// initialize components
+    document.addEventListener('DOMContentLoaded', function() {
+
+        //init carousel full width
+        $('.carousel.carousel-slider').carousel({
+            fullWidth:true,
+            indicators:true
+        });
+
+        //init nav section
+        $('.sidenav').sidenav();
+       
+
+});
