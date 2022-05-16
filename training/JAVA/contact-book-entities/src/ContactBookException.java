98626a81500869535f7eee4fd7c154d84d6f61b5
John Garcia <johnkegd@gmail.com>
2022-05-16 19:26:20 +0200

    removing training from devtools

--- training/JAVA/contact-book-entities/src/ContactBookException.java
+++
@@ -1,7 +0,0 @@
-public class ContactBookException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public ContactBookException(String description) {
-		super(description);
-	}
-}
