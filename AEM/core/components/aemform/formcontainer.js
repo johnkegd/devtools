a34f2e1aa18d229f738fc2eefe19ccca9c17ec52
John Garcia <johnkegd@gmail.com>
2022-05-16 21:51:30 +0200

    moving aem dir to repo for home submodule

--- AEM/core/components/aemform/formcontainer.js
+++
@@ -1,9 +0,0 @@
-/**
- * using property wcmmode and theme configured in af in aem.
- */
-use(function () {
-    var property;
-    for (property in this) {
-        request.setAttribute(property, this[property]);
-    }
-});
